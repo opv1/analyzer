5a2d36f5aff76fc8dd0c4199afaf7085803712c8
opv1 <pas7273@gmail.com>
2021-05-12 13:04:41 +0300

    more beauty

--- src/scripts/modules/FormateDate.js
+++ src/scripts/modules/FormateDate.js
@@ -4,8 +4,7 @@ export default class FormateDate {
   }
 
   formateDateLocal(date) {
-    const dateLocal = date.toLocaleString('ru', this.optionsDate)
-    return dateLocal
+    return date.toLocaleString('ru', this.optionsDate)
   }
 
   formateDateIco(date, days) {
@@ -13,7 +12,6 @@ export default class FormateDate {
       date.setDate(date.getDate() - days)
     }
 
-    const dateIco = date.toISOString().slice(0, 10)
-    return dateIco
+    return date.toISOString().slice(0, 10)
   }
 }
