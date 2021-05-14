ad83745a9627d7ce40f738e04f3d94b66b85d342
opv1 <pas7273@gmail.com>
2021-05-14 13:29:07 +0300

    more beauty

--- src/scripts/components/NewsCard.js
+++ src/scripts/components/NewsCard.js
@@ -1,5 +1,7 @@
 export default class NewsCard {
   createCard({ source, title, description, url, urlToImage, publishedAt }) {
+    const desc = description.replace(/^<ol>|<\/ol>|<li>|<\/li>/gi, '')
+
     this.template = `
       <a class="result__news" href="${url}" target="_blank" rel="noopener noreferrer">
         <div class="result__image">
@@ -8,7 +10,7 @@ export default class NewsCard {
         <div class="result__info">
           <span class="result__date">${publishedAt}</span>
           <h3 class="result__title">${title}</h3>
-          <p class="result__description">${description}</p>
+          <p class="result__description">${desc}</p>
           <span class="result__source">${source.name}</span>
         </div>
       </a>`
